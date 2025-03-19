3dbed237733ca1276187128aeb7a87b79b3d946e
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2025-03-19 15:56:42 +0100

    refactor: index directory code cleaning

--- public/index/handleVoice.js
+++
@@ -1,9 +0,0 @@
-import { peer } from "./main.js";
-
-const handleVoice = () => {
-  peer.on("stream", (stream) => {
-    console.log(stream);
-  });
-};
-
-export default handleVoice;
--- public/index/main.js
+++ public/index/main.js
@@ -4,25 +4,13 @@ import handleEnd from "./handleEnd.js";
 import handleSong from "./handleSong.js";
 import handleText from "./handleText.js";
 import handleUi from "./handleUi.js";
-import handleVoice from "./handleVoice.js";
 
 // https://github.com/devinekask/creative-code-4-s25/blob/main/webrtc/projects/p04-simple-peer/public/receiver.html
 let socket;
 export let peer;
 
-const servers = {
-  iceServers: [
-    {
-      urls: `stun:stun.l.google.com:19302`,
-    },
-  ],
-};
-
 const init = async () => {
   initSocket();
-  // $otherCamera.addEventListener("click", () => {
-  //   $otherCamera.play();
-  // });
   handleUi();
 };
 
@@ -54,8 +42,8 @@ const answerPeerOffer = async (myId, offer, peerId) => {
     const $game = document.querySelector(".game");
     $game.classList.remove("hidden");
   });
+
   handleCanvas();
-  handleVoice();
   handleText();
   handleSong();
   handleEnd();
